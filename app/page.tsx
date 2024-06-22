a24e3447035217a3dc2378887367fb692f024edd
Nic Smith <thesonicsmith@gmail.com>
2024-06-22 16:02:17 +1200

    Improve entry page

--- app/page.tsx
+++ app/page.tsx
@@ -12,5 +12,34 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function Home() {
-  return <div>Based Buds</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "#0050FF",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "row",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <img src={"/images/main.gif"} style={{ padding: 16 }} />
+        <a
+          href="https://zora.co/collect/base:0x0dc5ebca4a64e8a48672284e5404ad50bfed1735"
+          style={{
+            padding: 16,
+            textAlign: "center",
+            color: "white",
+          }}
+        >
+          Based Buds NFTs
+        </a>
+      </div>
+    </div>
+  );
 }
